f1252ac346d9ca990aa64055b6dd41bf2e233573
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-15 17:22:59 +0000

    global

---
+++ global.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Sept 2021
+// This Program global and local variable
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+  // This function calculate about local variable
+
+  int variableX = 10;
+  int variableY = 30;
+  int variableZ = variableX + variableY;
+  std::cout << "Local variable_x, variable_y, variable_z: "
+  << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+
+void globalVariable() {
+  // This function calculate about global variable
+
+  variableX = variableX + 1;
+  int variableY = 30;
+  int variableZ = variableX + variableY;
+  std::cout << "Global variable_x, variable_y, variable_z: "
+  << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+
+int main() {
+  // This function is about global and local variable
+
+  localVariable();
+  globalVariable();
+  std::cout << "\nDone" << std::endl;
+}
